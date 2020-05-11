9bf05c8b48ddb27ebaba3cd2f677ac95b1045994
anthonygabriele <anthonyjoeseph@gmail.com>
2020-05-11 16:10:04 -0400

    update rxjs-first-router

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,34 +1,46 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from '../components/App';
-import { createMockRouter } from 'rxjs-first-router';
+import { createMockRouter, Router } from 'rxjs-first-router';
 import configureStore from 'redux/Store';
 import { Provider } from 'react-redux';
+import AppAction from 'redux/AppAction';
+import { AppState } from 'redux/AppState';
+import { Store } from 'redux';
 
 describe('App Component', () => {
   it('renders learn react link', () => {
-    const router = createMockRouter('/');
+    const router: Router = createMockRouter();
+
+    // set the initial route
+    router.navigator.push('/show');
+
     const routeHistory: string[] = [];
-    const routeObs = router.route$;
-    routeObs.subscribe(r => routeHistory.push(r));
+    router.route$.subscribe((r: string) => routeHistory.push(r));
     
-    const store = configureStore(router);
-    expect(routeHistory[0] === '/').toBeTruthy();
+    const store: Store<AppState, AppAction> = configureStore(router);
     render(
       <Provider store={store}>
         <App />
       </Provider>
     );
 
-    const buttonElement = screen.getByText(/go to route/i);
-    fireEvent.click(buttonElement);
-    expect(routeHistory[1] === '/show').toBeTruthy();
+    expect(routeHistory[0] === '/show').toBeTruthy();
+    const fromRouteText = screen.getByText(/from route/i);
+    expect(fromRouteText).toBeInTheDocument();
 
-    const textElement = screen.getByText(/from button/i);
-    expect(textElement).toBeInTheDocument();
+    const goToLandingButton = screen.getByText(/go to landing/i);
+    fireEvent.click(goToLandingButton);
+    expect(routeHistory[1] === '/').toBeTruthy();
 
-    const buttonElementTwo = screen.getByText(/go to landing/i);
-    fireEvent.click(buttonElementTwo);
-    expect(routeHistory[2] === '/').toBeTruthy();
+    const landingText = screen.getByText(/landing/i);
+    expect(landingText).toBeInTheDocument();
+
+    const goToRouteButton = screen.getByText(/go to route/i);
+    fireEvent.click(goToRouteButton);
+    expect(routeHistory[2] === '/show').toBeTruthy();
+
+    const fromButtonText = screen.getByText(/from button/i);
+    expect(fromButtonText).toBeInTheDocument();
   });
 })
